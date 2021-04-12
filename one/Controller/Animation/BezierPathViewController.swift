ad5d6656eceb572715c667ed183ae7b34d63c6ab
Sidney.Li <sidneyli@augmentum.com.cn>
2021-04-12 16:54:52 +0800

    update bezier vc

--- one/Controller/Animation/BezierPathViewController.swift
+++ one/Controller/Animation/BezierPathViewController.swift
@@ -12,21 +12,54 @@ class BezierPathViewController: BaseViewController {
     lazy var scrollView = {
         return UIScrollView()
     }()
+    lazy var contentView = UIView()
+    lazy var firstView = UIView()
+    lazy var secondView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "贝塞尔曲线"
         setCustomNav()
-        self.view.addSubview(scrollView)
-        scrollView.backgroundColor = .lightGray
-        scrollView.snp.makeConstraints { (maker) in
-            maker.top.equalTo(navigationView.snp.bottom)
-            maker.leading.trailing.bottom.equalToSuperview()
-        }
+        initScrollContentView()
+        setupBezierView1()
+        setupBezierView2()
         // Do any additional setup after loading the view.
     }
     
+    func initScrollContentView() {
+        self.view.addSubview(scrollView)
+        scrollView.isScrollEnabled = true
+        scrollView.isUserInteractionEnabled = true
+        scrollView.snp.makeConstraints { (maker) in
+            maker.leading.trailing.bottom.equalToSuperview()
+            maker.top.equalTo(navigationView.snp.bottom)
+        }
+        scrollView.addSubview(contentView)
+        contentView.snp.makeConstraints { (maker) in
+            maker.top.bottom.equalToSuperview()
+            maker.leading.trailing.equalTo(self.view)
+        }
+    }
+    
     func setupBezierView1() {
-        
+        contentView.addSubview(firstView)
+        firstView.backgroundColor = .lightGray
+        let height = CGFloat(600)
+        firstView.snp.makeConstraints { (maker) in
+            maker.leading.trailing.top.equalToSuperview()
+            maker.height.equalTo(height)
+        }
+    }
+    
+    func setupBezierView2() {
+        contentView.addSubview(secondView)
+        secondView.backgroundColor = .lightText
+        let height = CGFloat(600)
+        secondView.snp.makeConstraints { (maker) in
+            maker.leading.trailing.equalToSuperview()
+            maker.top.equalTo(firstView.snp.bottom)
+            maker.height.equalTo(height)
+            maker.bottom.equalToSuperview()
+        }
     }
 }
